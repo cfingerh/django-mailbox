a15065dacb94a69e677be422cee92687d0eb1270
Christian <6157718+cfingerh@users.noreply.github.com>
2023-05-03 18:34:30 -0400

    Update signals.py

--- django_mailbox/signals.py
+++ django_mailbox/signals.py
@@ -1,3 +1,7 @@
 from django.dispatch.dispatcher import Signal
 
-message_received = Signal(providing_args=['message'])
+try:
+  message_received = Signal(providing_args=['message'])
+ except:
+  # Django 4
+  message_received = Signal()
